2cf113d7720cf1b7c77fa2515e6d35b753f82d61
Kamil Bentounes <45245189+kamil-bentounes@users.noreply.github.com>
2026-09-18 17:58:34 +0200

    deploy: lots 4, 5, 0c et 6

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/batchcooking/sw.js', { scope: '/batchcooking/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
